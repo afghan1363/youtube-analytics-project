47b24f794e8f9e9a1814d2c390f30244bdabec8a
Maxim Abavi <afghan1363@ya.ru>
2023-08-15 11:21:36 +0500

    дополняю класс

--- src/channel.py
+++ src/channel.py
@@ -6,13 +6,22 @@ class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YT_API_KEY')
 
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = self.channel["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+       # self.count_subscribers = self.channel[""]
+      #  count_videos
+       # count_views
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
