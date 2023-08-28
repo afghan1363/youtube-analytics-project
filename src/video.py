1a3afdeb40cc5d62fb94bca30fe4df6237b08d51
Maxim Abavi <afghan1363@ya.ru>
2023-08-28 22:46:05 +0500

    Решение домашнего задания 6

--- src/video.py
+++ src/video.py
@@ -8,19 +8,27 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=video_id).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        try:
+            self.video_id = video_id
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=video_id).execute()
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
+      #  else:
+
 
     def __str__(self):
         return self.video_title
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о видео."""
+        """Выводит в консоль информацию о видео. Вставил для экспериментов"""
         dict_to_print = self.video_response
         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
@@ -33,5 +41,5 @@ class PLVideo(Video):
 
 
 if __name__ == "__main__":
-    video1 = Video('AWX4JnAnjBE')
+    video1 = Video('AWX4JnAnjB11E')
     video1.print_info()
